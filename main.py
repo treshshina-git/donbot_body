d019fa70da8af3d84a3cfc91ee830ff6e49c23eb
treshshina-git <treshshina@gmail.com>
2026-06-09 16:43:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
 
-from aiogram import Bot
-from aiogram import Dispatcher
+from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
 
 from handlers.start import router as start_router
 from handlers.donate import router as donate_router
+from handlers.calculator import router as calc_router
 
 async def main():
 
@@ -21,34 +21,12 @@ dp = Dispatcher()
 
 dp.include_router(start_router)
 dp.include_router(donate_router)
+dp.include_router(calc_router)
+
+print("Бот запущен")
 
 await dp.start_polling(bot)
 ```
 
 if **name** == "**main**":
 asyncio.run(main())
-import asyncio
-
-from aiogram import Bot
-from aiogram import Dispatcher
-
-from config import BOT_TOKEN
-
-from handlers.start import router as start_router
-from handlers.donate import router as donate_router
-from handlers.calculator import router as calc_router
-
-async def main():
-
-    bot = Bot(BOT_TOKEN)
-
-    dp = Dispatcher()
-
-    dp.include_router(start_router)
-    dp.include_router(donate_router)
-    dp.include_router(calc_router)
-
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
