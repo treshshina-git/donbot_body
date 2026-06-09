fa1abcd6c9b36b66d1869cb6e59d8be82153fb50
treshshina-git <treshshina@gmail.com>
2026-06-09 16:48:48 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,17 +1,9 @@
 from aiogram import Router
 from aiogram.types import Message
-from aiogram.filters import CommandStart
-
-from keyboards.donate_kb import start_kb
 
 router = Router()
-@router.message()
-async def test(message: Message):
-    print("Получено:", message.text)
 
-@router.message(CommandStart())
-async def start(message: Message):
-    await message.answer(
-        "🧭 Donate Wizard\n\nНажмите кнопку ниже.",
-        reply_markup=start_kb
-    )
+@router.message()
+async def any_message(message: Message):
+    print("Получено:", message.text)
+    await message.answer("Я получил сообщение")
