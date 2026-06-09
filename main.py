df18beedfc29459c2ef6d5a2209904016230b323
treshshina-git <treshshina@gmail.com>
2026-06-09 12:59:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from handlers.start import router as start_router
 from handlers.donate import router as donate_router
 from handlers.calculator import router as calc_router
 
-
 async def main():
 
     bot = Bot(BOT_TOKEN)
@@ -21,7 +20,6 @@ async def main():
     dp.include_router(calc_router)
 
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
