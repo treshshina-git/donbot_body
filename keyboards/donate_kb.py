560ee5e540f56f5b7bef1ad9d185a2c95df673e2
treshshina-git <treshshina@gmail.com>
2026-06-09 16:06:21 +0200

    Update donate_kb.py

--- keyboards/donate_kb.py
+++ keyboards/donate_kb.py
@@ -1,64 +1,66 @@
-from aiogram.types import InlineKeyboardMarkup
-from aiogram.types import InlineKeyboardButton
-
-restart_kb = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(
-                text="🔄 Начать заново",
-                callback_data="start_donate"
-            )
-        ]
-    ]
+from aiogram.types import (
+InlineKeyboardMarkup,
+InlineKeyboardButton
 )
 
 start_kb = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(
-                text="🚀 Подобрать способ",
-                callback_data="start_donate"
-            )
-        ]
-    ]
+inline_keyboard=[
+[
+InlineKeyboardButton(
+text="🚀 Подобрать способ",
+callback_data="start_donate"
+)
+]
+]
 )
 
 donation_type_kb = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(
-                text="❤️ Регулярно",
-                callback_data="regular"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="⚡ Быстро",
-                callback_data="fast"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="🎁 Просто поблагодарить",
-                callback_data="thanks"
-            )
-        ]
-    ]
+inline_keyboard=[
+[
+InlineKeyboardButton(
+text="❤️ Регулярно",
+callback_data="regular"
+)
+],
+[
+InlineKeyboardButton(
+text="⚡ Быстро",
+callback_data="fast"
+)
+],
+[
+InlineKeyboardButton(
+text="🎁 Просто поблагодарить",
+callback_data="thanks"
+)
+]
+]
 )
 
 telegram_kb = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(
-                text="Да",
-                callback_data="tg_yes"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="Нет",
-                callback_data="tg_no"
-            )
-        ]
-    ]
+inline_keyboard=[
+[
+InlineKeyboardButton(
+text="✅ Да",
+callback_data="tg_yes"
+)
+],
+[
+InlineKeyboardButton(
+text="❌ Нет",
+callback_data="tg_no"
+)
+]
+]
+)
+
+restart_kb = InlineKeyboardMarkup(
+inline_keyboard=[
+[
+InlineKeyboardButton(
+text="🔄 Начать заново",
+callback_data="start_donate"
+)
+]
+]
 )
