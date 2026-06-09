19bf1cbecd7b77d0788100ec9f56da174fd2c5d4
treshshina-git <treshshina@gmail.com>
2026-06-09 16:08:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,35 @@ from config import BOT_TOKEN
 
 from handlers.start import router as start_router
 from handlers.donate import router as donate_router
+
+async def main():
+
+```
+bot = Bot(BOT_TOKEN)
+
+await bot.delete_webhook(
+    drop_pending_updates=True
+)
+
+dp = Dispatcher()
+
+dp.include_router(start_router)
+dp.include_router(donate_router)
+
+await dp.start_polling(bot)
+```
+
+if **name** == "**main**":
+asyncio.run(main())
+import asyncio
+
+from aiogram import Bot
+from aiogram import Dispatcher
+
+from config import BOT_TOKEN
+
+from handlers.start import router as start_router
+from handlers.donate import router as donate_router
 from handlers.calculator import router as calc_router
 
 async def main():
