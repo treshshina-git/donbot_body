f5d4284ddb032e3e8122032d7e57f634708a71ba
treshshina-git <treshshina@gmail.com>
2026-06-09 16:36:40 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -6,20 +6,10 @@ from keyboards.donate_kb import start_kb
 
 router = Router()
 
+
 @router.message(CommandStart())
 async def start(message: Message):
-
-```
-await message.answer(
-    """
-```
-
-🧭 Donate Wizard
-
-Помогу подобрать самый удобный
-способ поддержки автора.
-
-Нажмите кнопку ниже.
-""",
-reply_markup=start_kb
-)
+    await message.answer(
+        "🧭 Donate Wizard\n\nНажмите кнопку ниже.",
+        reply_markup=start_kb
+    )
