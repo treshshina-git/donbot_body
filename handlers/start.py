f64adb030b7e8d3d181b1ae9f5b1c7dc88281e72
treshshina-git <treshshina@gmail.com>
2026-06-11 07:32:10 +0200

    Delete handlers directory

--- handlers/start.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router
-from aiogram.types import Message
-
-router = Router()
-
-@router.message()
-async def any_message(message: Message):
-    print("Получено:", message.text)
-    await message.answer("Я получил сообщение")
