42541259b310ba74354d429a0847c79409886a41
treshshina-git <treshshina@gmail.com>
2026-06-11 08:29:01 +0200

    Update github_api.py

--- github_api.py
+++ github_api.py
@@ -6,22 +6,15 @@ REPO = "donbot_body"
 
 
 async def get_files(folder: str):
-    path = quote(f"ROMS/{folder}")
-
     url = (
-        f"https://api.github.com/repos/"
-        f"{OWNER}/{REPO}/contents/{path}"
+        "https://api.github.com/repos/"
+        "treshshina-git/donbot_body/contents"
     )
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             data = await resp.json()
 
-    if not isinstance(data, list):
-        raise Exception(f"GitHub returned error: {data}")
+    print(data)
 
-    return [
-        item["name"]
-        for item in data
-        if item["type"] == "file"
-    ]
+    return []
