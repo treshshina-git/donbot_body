c1e047e7e233b040181d85212164bdd5f0b89ad8
treshshina-git <treshshina@gmail.com>
2026-06-11 08:45:34 +0200

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,16 +1,40 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+FILES_CACHE = {}
+
+
 def consoles_kb():
     kb = InlineKeyboardBuilder()
-    for c in ("Nintendo", "Super Nintendo", "SEGA"):
-        kb.button(text=c, callback_data=f"console:{c}")
+
+    for c in ("NES", "SNES", "SEGA"):
+        kb.button(
+            text=c,
+            callback_data=f"console:{c}"
+        )
+
     kb.adjust(1)
+
     return kb.as_markup()
 
-def files_kb(console, files):
+
+def files_kb(user_id, console, files):
     kb = InlineKeyboardBuilder()
-    for f in files:
-        kb.button(text=f, callback_data=f"rom:{console}:{f}")
-    kb.button(text="⬅️ Back", callback_data="back")
+
+    FILES_CACHE[user_id] = {}
+
+    for i, file in enumerate(files):
+        FILES_CACHE[user_id][str(i)] = (console, file)
+
+        kb.button(
+            text=file[:50],
+            callback_data=f"rom:{i}"
+        )
+
+    kb.button(
+        text="⬅️ Back",
+        callback_data="back"
+    )
+
     kb.adjust(1)
+
     return kb.as_markup()
