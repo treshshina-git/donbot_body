c22f3eb42254725b1c6691b5b988f4c05057408b
treshshina-git <treshshina@gmail.com>
2026-06-11 07:07:09 +0200

    Delete States/donate_states.py

--- States/donate_states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State
-from aiogram.fsm.state import StatesGroup
-
-
-class DonateWizard(StatesGroup):
-    donation_type = State()
-    telegram_usage = State()
-    coffee_amount = State()
